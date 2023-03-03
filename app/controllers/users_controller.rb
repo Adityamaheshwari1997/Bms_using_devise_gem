c9f7e2076602a5e9bf2f2f1d0bd1f2d7172b02df
Aditya Maheshwari <Adityamaheshwari1997>
2023-03-03 12:00:54 +0530

    Remove Theater_id from user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:user_id])
-    @theater = Theater.find(params[:theater_id])
     @movie = Movie.find(params[:movie_id])
   end
 
